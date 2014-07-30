813f5365a63f3305e9749743d51cf201cbe87b51
ThomasDebek <tom.debek@gmail.com>
2014-07-30 13:40:59 +0200

    Dajmy sciezke odpowiednia w routsach

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  
-  get 'store/index'
 
   resources :places
 
   root 'static_pages#home'
+  match '/index', to: 'store#index', via: 'get'
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
