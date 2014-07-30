ffc09afe26936d06a936d7ee7dbdf2d7796300e9
ThomasDebek <tom.debek@gmail.com>
2014-07-30 14:00:23 +0200

    rails g scaffold cart, rake db:migrate. Dodajmy mozliwosc sprzedarzy naszych produktow

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :carts
+
   resources :places
 
   root 'static_pages#home'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140730112253) do
+ActiveRecord::Schema.define(version: 20140730115935) do
+
+  create_table "carts", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "places", force: true do |t|
     t.string   "title"
